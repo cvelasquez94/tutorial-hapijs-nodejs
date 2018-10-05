a49f5f5c702f1abe0d2626f9a3c97b7dc57d8daa
Carlos <carlosbrunotbc@gmail.com>
2018-10-04 23:07:39 -0300

    add routes for PUT and DELETE

--- routes.js
+++ routes.js
@@ -10,6 +10,49 @@ module.exports = {
 					handler: async (req, res) => {
 						return 'Hi my friend';
 					}
+				}, {
+					method: 'GET',
+					path: '/user/{name?}',
+					handler: async (req, res) => {
+						const name = (req.params.name) ? req.params.name : 'envited';
+						return `Hi ${name}`;
+					}
+				}, {
+					method: 'POST',
+					path: '/newUser',
+					handler: async (req, res) => {
+						const newUser = {
+							name: req.payload.name,
+							lastName: req.payload.lastName
+						};
+						return res.response({
+							data: newUser
+						}).type('application/json');
+
+					}
+				}, {
+					method: 'PUT',
+					path: '/user/{id}',
+					handler: async (req, res) => {	
+						const { id } = req.params;
+						const newUser = {
+							name: req.payload.name,
+							lastName: req.payload.lastName
+						};
+						return res.response({
+							datos: newUser,
+							message: `User ID: ${id} successfully modified`
+						}).type('application/json');
+					}
+				}, {
+					method: 'DELETE',
+					path: '/user/{id}',
+					handler: async (req, res) => {
+						const { id } = req.params;
+						return res.response({
+							message: `User ID: ${id} successfully removed`
+						}).type('application/json');
+					} 
 				}	
 			]);
 		}
